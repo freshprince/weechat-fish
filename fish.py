freshprince/weechat-fish

commit 6eb9b8488166ca1c0f82d0f9d025af488d973d06

fixes decryption of messages that were too long and were truncated by the server
Thanks to mumixam <mumixam@gmail.com>

fish.py
@@ -688,7 +688,10 @@ def fish_modifier_in_332_cb(data, modifier, server_name, string):
         fish_cyphers[target] = b
     else:
         b = fish_cyphers[target]
-    clean = blowcrypt_unpack(match.group(3), b)
+
+    msg = match.group(3)[:-(len(match.group(3)) % 12)]
+
+    clean = blowcrypt_unpack(msg, b)
 
     fish_announce_encrypted(buffer, target)
 

commit 6047cbafc407544b9f827848aefeda225f58216f

fixed msg truncated decrypting
This fixes a bug David introduced in commit
6eb9b8488166ca1c0f82d0f9d025af488d973d06

fish.py
@@ -257,6 +257,9 @@ def blowcrypt_unpack(msg, cipher):
     if len(rest) < 12:
         raise MalformedError
 
+    if not (len(rest) %12) == 0:
+        rest = rest[:-(len(rest) % 12)]
+
     try:
         raw = blowcrypt_b64decode(padto(rest, 12))
     except TypeError:
@@ -691,9 +694,7 @@ def fish_modifier_in_332_cb(data, modifier, server_name, string):
     else:
         b = fish_cyphers[target]
 
-    msg = match.group(3)[:-(len(match.group(3)) % 12)]
-
-    clean = blowcrypt_unpack(msg, b)
+    clean = blowcrypt_unpack(match.group(3), b)
 
     fish_announce_encrypted(buffer, target)
 